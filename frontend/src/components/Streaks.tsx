080158b0a8b1196fb47274867435ad6e9ae43e13
Dylan Morrison <dylanjmorrison30@gmail.com>
2022-08-19 10:45:22 +0100

    frontend changes

--- frontend/src/components/Streaks.tsx
+++ frontend/src/components/Streaks.tsx
@@ -8,7 +8,7 @@ const Streaks = () => {
   console.log(streaks);
 
   return (
-    <div className="col-span-3 max-w-[500px]  sm:mx-10 sm:grid-cols-2 xl:ml-30 xl:gap-y-4 xl: gap-8 lg:mt-0 mt-10  sm:grid   flex flex-col ">
+    <div className="col-span-3 xl:max-w-[500px]  justify-center sm:mx-10 sm:grid-cols-2 xl:ml-30 xl:gap-y-4 xl: gap-8 lg:mt-0 mt-10  sm:grid   flex flex-col ">
       {streaks.map((streak) => (
         <Streak key={streak.streakId} streak={streak} />
       ))}
